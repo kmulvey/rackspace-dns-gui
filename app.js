dab8e01404b1df821457cdf1b856adb764ded7fa
Kevin <king.feruke@gmail.com>
2012-03-03 17:34:36 -0500

    ping6

--- app.js
+++ app.js
@@ -156,6 +156,31 @@ app.post('/nslookup', function(req, res){
 	});
 });
 
+app.get('/ping', function(req, res){
+	res.render('ping');
+});
+app.post('/ping', function(req, res){
+	var output = "";
+	if(req.body.v6 == 'on'){
+		dig = spawn('ping6', [ '-c 5', req.body.host ]);
+	}
+	else{
+		dig = spawn('ping', [ '-c 5', req.body.host ]);
+	}
+	dig.stdout.on('data', function(data) {
+		output += data;
+	});
+	dig.stderr.on('end', function(data) {
+		res.render('tools-result', {result: output});
+	});
+	dig.stderr.on('close', function(data) {
+		res.render('tools-result', {result: output});
+	});
+	dig.stderr.on('error', function(data) {
+		res.render('tools-result', {result: 'ERROR: ' + data});
+	});
+});
+
 app.post('/login', authenticate, routes.index);
 
 app.listen(3000);
