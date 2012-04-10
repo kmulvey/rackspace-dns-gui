b412b8f481d9cc3c0f8e1a77101318e92e35fb4a
Kevin Mulvey <kevin@kevinmulvey.net>
2012-04-09 20:33:09 -0400

    many changes to the details page and Record iteration

--- Record.js
+++ Record.js
@@ -10,8 +10,13 @@ Record.prototype.init = function(arg) {
 	this.ttl = arg.ttl;
 	this.updated = arg.updated;
 	this.created = arg.created;
-}
-
+};
+Record.prototype.initAll = function(name, type, data, ttl) {
+	this.name = name;
+	this.type = type;
+	this.data = data;
+	this.ttl = ttl;
+};
 Record.prototype.initAll = function(name, id, type, data, ttl, updated, created) {
 	this.name = name;
 	this.id = id;
@@ -20,23 +25,27 @@ Record.prototype.initAll = function(name, id, type, data, ttl, updated, created)
 	this.ttl = ttl;
 	this.updated = updated;
 	this.created = created;
-}
-
+};
 Record.prototype.toJSON = function() {
-	var output = '{ "name" : "' + this.name + '", "id" : "' + this.id + '", "type" : "' + this.type + '", ';
-	output = output + '"data" : "' + this.data + '", "ttl" : "' + this.ttl + '", ';
-	output = output + '"updated" : "' + this.updated + '", "created" : "' + this.created + '" }';
+	var output = '{';
+	for ( var key in this) {
+		if(typeof this[key] != 'function' && this[key] != undefined && this[key] != null){
+			console.log(key + ' is ' + typeof this[key] + ' = ' + this[key]);
+			output += '"' + key + '": "' + this[key] + '",';
+		}
+	}
+	output = output.substring(0, output.length -2);
+	output += '}';
 	return output;
-}
+};
 
 Record.prototype.addRequest = function() {
-        var output = '{ "name" : "' + this.name + '", "data" : "' + this.data + '", "ttl" : "' + this.ttl + '" }';
-        return output;
-}
+	var output = '{ "name" : "' + this.name + '", "data" : "' + this.data + '", "ttl" : "' + this.ttl + '" }';
+	return output;
+};
 
 Record.prototype.modifyRequest = function() {
-        var output = '{ "name" : "' + this.name + '", "id" : "' + this.id + '", ';
-        output = output + '"data" : "' + this.data + '", "ttl" : "' + this.ttl + '" }';
-        return output;
-}
-
+	var output = '{ "name" : "' + this.name + '", "id" : "' + this.id + '", ';
+	output = output + '"data" : "' + this.data + '", "ttl" : "' + this.ttl + '" }';
+	return output;
+};
